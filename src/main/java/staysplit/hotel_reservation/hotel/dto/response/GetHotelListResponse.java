68aacb9401dbc30e0bfdda16dabab8063ac130c5
miji <mjkim2304@gmail.com>
2025-12-16 15:13:14 +0900

    feat: 호텔 검색 기능에 bounding box 추가하여 후보군 1차 필터링 및 쿼리 분리

--- src/main/java/staysplit/hotel_reservation/hotel/dto/response/GetHotelListResponse.java
+++ src/main/java/staysplit/hotel_reservation/hotel/dto/response/GetHotelListResponse.java
@@ -1,8 +1,5 @@
 package staysplit.hotel_reservation.hotel.dto.response;
 
-import staysplit.hotel_reservation.hotel.entity.HotelEntity;
-import staysplit.hotel_reservation.photo.service.PhotoUrlBuilder;
-
 public record GetHotelListResponse(
         Integer hotelId,
         String name,
@@ -11,16 +8,4 @@ public record GetHotelListResponse(
         Double rating,
         Integer reviewCount,
         String mainImageUrl
-) {
-    public static GetHotelListResponse toDto(HotelEntity hotel, PhotoUrlBuilder photoUrlBuilder) {
-        return new GetHotelListResponse(
-                hotel.getId(),
-                hotel.getName(),
-                hotel.getAddress(),
-                hotel.getStarLevel(),
-                hotel.getRating(),
-                hotel.getReviewCount(),
-                hotel.getMainPhoto().map(photo -> photo.buildFullUrl(photoUrlBuilder)).orElse(null)
-        );
-    }
-}
+) {}
--- src/main/java/staysplit/hotel_reservation/hotelSearch/repository/HotelSearchRepositoryIml.java
+++ src/main/java/staysplit/hotel_reservation/hotelSearch/repository/HotelSearchRepositoryIml.java
@@ -16,7 +16,6 @@ import staysplit.hotel_reservation.hotel.entity.HotelEntity;
 import staysplit.hotel_reservation.hotel.entity.QHotelEntity;
 import staysplit.hotel_reservation.hotelSearch.dto.request.HotelSearchCondition;
 import staysplit.hotel_reservation.reservation.domain.entity.QReservationEntity;
-import staysplit.hotel_reservation.reservation.domain.entity.ReservationEntity;
 import staysplit.hotel_reservation.reservedRoom.entity.QReservedRoomEntity;
 import staysplit.hotel_reservation.room.domain.QRoomEntity;
 
@@ -32,8 +31,6 @@ public class HotelSearchRepositoryIml implements HotelSearchRepository {
     private static final double SEARCH_RADIUS = 5.0; // 검색할 반경 (km)
 
     private static final QHotelEntity HOTEL = QHotelEntity.hotelEntity;
-    private static final QReservedRoomEntity RESERVED_ROOM = QReservedRoomEntity.reservedRoomEntity;
-    private static final QReservationEntity RESERVATION = QReservationEntity.reservationEntity;
     private static final QRoomEntity ROOM = QRoomEntity.roomEntity;
 
     @Override
@@ -45,26 +42,30 @@ public class HotelSearchRepositoryIml implements HotelSearchRepository {
         NumberExpression<Double> distance = calculateDistance(condition.getLongitude(), condition.getLatitude());
         builder.and(distance.loe(SEARCH_RADIUS));
 
-        List<HotelEntity> content = queryFactory
-                .selectDistinct(HOTEL)
+        // Radius 안의 호텔 검색
+        List<Integer> hotelIdsWithinDistance = queryFactory
+                .select(HOTEL.id)
                 .from(HOTEL)
                 .join(ROOM).on(ROOM.hotel.eq(HOTEL))
-                .leftJoin(RESERVATION).on(RESERVATION.hotel.eq(HOTEL))
-                .leftJoin(RESERVED_ROOM).on(RESERVED_ROOM.reservation.eq(RESERVATION))
                 .where(builder)
+                .groupBy(HOTEL.id)       // 호텔 단위로 묶어서 중복 제거
                 .orderBy(distance.asc()) // 거리 순 정렬
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
         Long total = queryFactory
-                .select(HOTEL.countDistinct())
+                .select(HOTEL.id.countDistinct())
                 .from(HOTEL)
                 .join(ROOM).on(ROOM.hotel.eq(HOTEL))
-                .leftJoin(RESERVATION).on(RESERVATION.hotel.eq(HOTEL))
-                .leftJoin(RESERVED_ROOM).on(RESERVED_ROOM.reservation.eq(RESERVATION))
                 .where(builder)
                 .fetchOne();
+
+        // hotel 20개만 상세조회
+        List<HotelEntity> content = queryFactory
+                .selectFrom(HOTEL)
+                .where(HOTEL.id.in(hotelIdsWithinDistance))
+                .fetch();
 
         return new PageImpl<>(content, pageable, total == null ? 0 : total);
     }
@@ -72,7 +73,8 @@ public class HotelSearchRepositoryIml implements HotelSearchRepository {
     // 조건 조합: 체크인, 체크아웃 날짜, 인원, 가격, 별점 계산  (거리는 없음)
     private BooleanBuilder buildSearchPredicate(HotelSearchCondition condition) {
         BooleanBuilder builder = new BooleanBuilder();
-        builder.and(availableBetween(condition.getCheckIn(), condition.getCheckOut()));
+        builder.and(boundingBox(condition.getLongitude(), condition.getLatitude()));    // bounding box
+        builder.and(availableBetween(condition.getCheckIn(), condition.getCheckOut())); // overlap
         builder.and(numGuestGoe(condition.getNumGuest()));
         builder.and(minPrice(condition.getMinPrice()));
         builder.and(maxPrice(condition.getMaxPrice()));
@@ -94,6 +96,20 @@ public class HotelSearchRepositoryIml implements HotelSearchRepository {
 
         // 두 지점간의 거리를 m에서 km로 변환
         return distanceMeter.divide(1000.0);
+    }
+
+    // Bounding Box
+    public BooleanExpression boundingBox(Double givenLongitude, Double givenLatitude) {
+        double latDelta = SEARCH_RADIUS / 110.0;
+        double lonDelta = SEARCH_RADIUS / (110.0 * Math.cos(Math.toRadians(givenLatitude)));
+
+        double minLat = givenLatitude - latDelta;
+        double maxLat = givenLatitude + latDelta;
+        double minLon = givenLongitude - lonDelta;
+        double maxLon = givenLongitude + lonDelta;
+
+        return HOTEL.latitude.between(minLat, maxLat).and(
+                HOTEL.longitude.between(minLon, maxLon));
     }
 
     // 주어진 날짜 구간에 겹치는 예약이 없는 ROOM만 true
